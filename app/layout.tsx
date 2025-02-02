3a61fd39c87ca3e3f9698a78c4868c957098a35a
levanse <levansePro@gmail.com>
2025-02-02 23:37:13 +0100

    feat: add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 
 import "./globals.css";
+import { Header } from "@/components/shared/header";
 
 const nunito = Nunito({
   subsets: ["cyrillic"],
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <Header />
         <main className="min-h-screen">{children}</main>
       </body>
     </html>
