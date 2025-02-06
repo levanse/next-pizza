4e1dd97434bdcd105dd3e275f7251e9f4bd94eff
levanse <levansePro@gmail.com>
2025-02-06 17:19:11 +0100

    feat: create component TopBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import { Categories, Container, SortPopup, Title } from "@/components/shared";
+import { Container, Title } from "@/components/shared";
+import { TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
-        <Categories />
-        <SortPopup />
       </Container>
+
+      <TopBar />
     </>
   );
 }
