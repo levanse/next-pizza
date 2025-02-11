fe3d8b4f8a346587771d9417344ad8afb6251529
levanse <levansePro@gmail.com>
2025-02-11 12:31:07 +0100

    refactor: refactor components Checkbox, Filters, Home

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -10,11 +10,7 @@ interface Props {
 export const Filters: React.FC<Props> = ({ className }) => {
   return (
     <div className={className}>
-      <Title
-        text="Фильтрация"
-        size="sm"
-        className="mb-5 font-bold pb-4 border-b border-b-neutral-100"
-      />
+      <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
 
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можно собирать" value="1" />
